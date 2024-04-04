32a0641849ca7275d2b3b1cc1efe228d9cfad95b
DESKTOP-FI6B674\admin <dangquocnhat1998@gmail.com>
2024-04-04 22:13:06 +0700

    edit gitignore

---
+++ src/app.js
@@ -0,0 +1,24 @@
+const compression = require("compression");
+const express = require("express");
+const { default: helmet } = require("helmet");
+const morgan = require("morgan");
+const app = express();
+
+//init middlewares
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(compression());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//init db
+require("./dbs/init.mongodb");
+// const { checkOverLoad } = require("./helpers/check.connect");
+// checkOverLoad();
+
+//init routes
+app.use('/', require("./routes"))
+
+//init error handler
+
+module.exports = app;
