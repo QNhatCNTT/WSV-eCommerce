780f1ed89cd703ad3cfbb8f0aa8a20e7e7ac055e
DESKTOP-GTDLRMV\dangq <nhatdq@dtp-education.com>
2024-04-05 08:47:48 +0700

    handle success response

--- src/controllers/access.controller.js
+++ src/controllers/access.controller.js
@@ -2,10 +2,14 @@
 
 const express = require("express");
 const AccessService = require("../services/access.service");
+const { CREATED } = require("../core/success.response");
 
 class AccessController {
     signUp = async (req, res, next) => {
-        return res.status(201).json(await AccessService.signUp(req.body));
+        new CREATED({
+            message: "Registered OK!",
+            metadata: await AccessService.signUp(req.body),
+        }).send(res);
     };
 }
 
